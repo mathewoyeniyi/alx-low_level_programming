63930f7908e1a08d53c96ea06fed27bbd5012215
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-09 05:19:38 -0700

    Commit tash 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,17 +18,17 @@ int main(void)
 
 if  (n % 10 > 5)
 {
-	printf("Last digit of %i and %i is greater than 5\n", n, n % 10);
+	printf("Last digit of %i and is greater than 5\n", n, n % 10);
 }
 
 else if (n % 10 == 0)
 {
-	printf("Last digit of %i is %i and is 0\n", n, n % 10);
+	printf("Last digit of %i is and is 0\n", n, n % 10);
 }
 
 else
 {
-	printf"Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
+	printf"Last digit of %i is and is less than 6 and not 0\n", n, n % 10);
 }
 
 					return (0);
