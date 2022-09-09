fd4e9a70b9ae2be2f7b1e2bed2d786342975d016
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-09 06:34:18 -0700

    Commit task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-			printf("%n is positive\n", n);
+			printf("%i is positive\n", n);
 	else if (n == 0)
-			printf("%n is zero\n", n);
+			printf("%i is zero\n", n);
 	else
-			printf("%ni is negative\n", n);
+			printf("%i is negative\n", n);
 		return (0);
 }
