ac47817c03f882600c50f5b7dea9ab8d416efa7c
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-09 07:49:59 -0700

    commit task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Rfeturen: 0
+ */
+
+int main(0)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
