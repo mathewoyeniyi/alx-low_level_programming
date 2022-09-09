d791557967c54db10def6d4a3b5bcef7833eb54e
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-09 08:31:26 -0700

    commit task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: 0 Aways (success/correct)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
