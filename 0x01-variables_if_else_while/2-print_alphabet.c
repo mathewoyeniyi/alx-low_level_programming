3ead48bd9ee029b684494e298ab9c9157866d6d1
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-09 07:13:59 -0700

    Commit task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success/correct)
+ */  
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch)
+	}
+	putchar('\n')
+		return (0);
+}
