7e857480b8219653f17741d65a783dc0de592b0e
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-10 04:42:22 +0100

    commit task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: 'the program dis'
+ * Return: 0 Aways (success)
+ */
+
+int main(void)
+{
+	int i, p, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (p = 1; p <= 9; p++)
+		{
+			for (m = 2; m <= 9; m++)
+			{
+				if (m > p && p > i)
+				{
+					putchar(i + '0');
+					putchar(p + '0');
+					putchar(m + '0');
+					{
+						if (j != 7)
+						{
+							putchar(',');
+							putchar(' ');
+						}}}}}}
+	putchar('\n');
+	return (0);
+}
