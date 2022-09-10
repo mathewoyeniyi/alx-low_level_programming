7d16dd8aa6ce2438c589b0a68fef66e6905d7995
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-10 04:50:02 +0100

    commit task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,11 +19,11 @@ int main(void)
 			{
 				if (m > p && p > i)
 				{
-					putchar(i + '0');
-					putchar(p + '0');
-					putchar(m + '0');
-					{
-						if (i != 7)
+				putchar(i + '0');
+				putchar(p + '0');
+				putchar(m + '0');
+			{
+					if (i != 7)
 						{
 							putchar(',');
 							putchar(' ');
