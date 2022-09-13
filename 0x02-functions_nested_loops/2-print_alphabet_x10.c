65171f1b1cded63eee200cb6aa1e3e0346f31124
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-13 09:07:09 -0700

    Committing task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function of abc
+ *
+ * Return: 0 Always
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	if (c = 0; c <= 9; c++)
+	{
+		if (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+	}
+}
