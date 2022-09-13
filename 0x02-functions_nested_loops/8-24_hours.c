1d7469e863ef5fef9350dcb02e330f90a79214d8
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-13 11:18:44 -0700

    Committing task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,8 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-		while (minutes <= 59)
-		{
+	while (minutes <= 59)
+	{
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
@@ -26,9 +26,9 @@ void jack_bauer(void)
 	_putchar(minutes / 10 + '0');
 	_putchar(mins_remainder + '0');
 	minutes++;
-		_putchar('\n');
-		}
-		hours++;
-		minutes = 0;
+	_putchar('\n');
+	}
+	hours++;
+	minutes = 0;
 	}
 }
