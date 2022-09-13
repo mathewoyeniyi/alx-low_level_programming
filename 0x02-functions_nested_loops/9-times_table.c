1eb610f070fe4a745af75e945e7de53c3d032490
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-13 11:43:25 -0700

    Committing task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,8 +21,8 @@ void times_table(void)
 				prod = num * mult;
 				if (prod <= 9)
 					_putchar(' ');
-				else _purtchar((prod / 10) + '0');
-				_putchar((pro % 10) + '0');
+				else _putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 	}
