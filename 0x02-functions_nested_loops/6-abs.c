11f22b7b06e0a1cc48fbd354be92d19c8612dfbd
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-13 10:24:20 -0700

    Committing task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _abs - Write a function that computes the absolute value of an integer
+ *
+ * return: Aways 0.
+ */
+
+int _abs(int c);
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	return (c * -1);
+}
