1b9bc17bfcde7330a2705940db1fb6a1f7b1ec2b
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-13 07:24:57 -0700

    Commit task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - no argument
+ *
+ * Description -  prints _putchar, followed by a new line.
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	printf("_putchar");
+
+	printf("\n");
+
+		return (0);
+}
