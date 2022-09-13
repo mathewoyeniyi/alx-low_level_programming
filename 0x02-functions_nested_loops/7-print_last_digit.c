69d71c3dbcad61169d6a75da4728459a8ea3f8b2
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-13 10:40:37 -0700

    Committing task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_last_digit: - function that prints the last digit of a number
- *
+ * print_last_digit - function that prints the last digit of a number
+ *print_last_digit:
  * @n: Number that is pass to int print_last_digit int
  * Retur: Aways 0.
  */
