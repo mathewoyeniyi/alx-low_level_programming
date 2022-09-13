02923f96e941e2160305a699f559f279a98d05bb
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-13 09:57:48 -0700

    Committing task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Write a function that prints the sign of a number
+ *
+ * @n: Number to pass to print sign
+ * Return: Always 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+
+		else if (n < 0)
+		{
+			_purchar('-');
+			return (-1)
+		}
+		else 
+		{
+			_putchar('0');
+			return (0);
+		}
+	}
+}
