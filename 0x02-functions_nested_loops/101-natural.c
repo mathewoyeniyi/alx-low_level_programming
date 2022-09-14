e88ed093f3dab8ce95130d0e1d5d78d0d4eaffb6
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-14 02:39:02 -0700

    Committing task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,21 +9,16 @@
  * Return: Always 0.
  */
 
-int main()
+int main(void)
 {
-	int num;
+	int i;
 	int sum = 0;
-
-	printf("Enter number: ");
-	scanf("%d", &num);
-
-	for (int i = 0; i < num; i++)
-	{
-		if (i % 3 == 0)
-			sum = sum + i;
-		else if (i % 5 == 0)
-			sum = sum + i;
-	}
-	printf("The sum of all multiples of 3 or 5 below %d = %d\n", num, sum);
-	return (0);
+	
+		for (i =0; i < 1024; i++)
+		{
+			if ((i % 3) == 0 || (i % 5) == 0)
+				sum +=i;
+		}
+		printf("%d\n", sum};
+		return (0);
 }
