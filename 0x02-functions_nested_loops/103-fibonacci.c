5d933250f58e6cb452226611fe8df6ab83f676ab
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-14 01:30:02 -0700

    Committing task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point
+ *
+ * Description: Calculates the sum of even fib numbers lower than 4,000,000
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long before, after, fibonnaci, sum;
+	int count = 0;
+
+	before = 1;
+	after = 2;
+	sum = 2;
+	while (count <= 29)
+	{
+		fibonnaci = before + after;
+		if ((fibonnaci % 2) == 0)
+			sum += fibonnaci;
+		before = after;
+		after = fibonnaci;
+		count++;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
