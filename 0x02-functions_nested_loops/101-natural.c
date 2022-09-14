15e14d8a7e976cdc5ee01d39e9fdf2a0a122cb29
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-14 02:44:28 -0700

    Committing task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,10 +14,10 @@ int main(void)
 	int i;
 	int sum = 0;
 	
-		for (i =0; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 		{
 			if ((i % 3) == 0 || (i % 5) == 0)
-				sum +=i;
+				sum += i;
 		}
 		printf("%d\n", sum);
 		return (0);
