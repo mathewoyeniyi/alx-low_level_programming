f8f0e59a842fd21132151705843b8fb911400d09
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-15 10:46:05 -0700

    Committing task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - prints positive or negative integer
  *
- * Return: Always 0.
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
-
 	i = 0;
-	positive_or_negative(i);
 
-	return(0);
+	positive_or_negative(i);
+	return (0);
 }
