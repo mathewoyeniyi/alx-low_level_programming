723e6925ab7e966a0a96007796e5e5d0b8510e2b
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-15 09:18:54 -0700

    committing task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char digits;
+
+	for (digits = '0'; digits <= '9'; digits++)
+	{
+		_putchar(digit)
+	}
+	_putchar('\n');
+}
