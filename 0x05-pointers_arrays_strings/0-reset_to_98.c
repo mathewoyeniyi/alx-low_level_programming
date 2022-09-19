1303ec10727bbbc7a4867ff46ddbfc4d4fbb1aff
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-19 08:27:55 -0700

    Committing task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,8 @@
  * Description -reset_to_98 - function that takes a pointer to an int as
  * parameter and updates the value it points to to 98
  * 
- * Return: Always 0.
+ * @n: This is the entry
+ *
  */
 
 void reset_to_98(int *n)
