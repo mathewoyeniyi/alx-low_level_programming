d8eecb59d5fd3085d6d235aec4e845db511593e8
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-19 09:26:52 -0700

    Committing task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - This print every 2 casillas
+ *
+ * @s: THis is my entry
+ *
+ */
+
+void puts2(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(s[a]);
+		}
+	}
+	_putchar('\n');
+}
