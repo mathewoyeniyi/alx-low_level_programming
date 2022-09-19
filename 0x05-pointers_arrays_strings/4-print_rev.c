8b83dfdea1dff4fc53b355cd490ff61c269e13ea
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-19 09:08:24 -0700

    Committing task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,14 @@
 /**
  * print_rev - This function prints a reverse string
  *
- * @s: This is the input string
+ * @s: input string
  */
 
 void print_rev(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; index--)
+	for (index = 0; s[index] != '\0'; index++)
 		;
 	for (index = index - 1; s[index] != '\0'; index--)
 	{
