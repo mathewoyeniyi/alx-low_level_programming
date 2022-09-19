12901f0e86af82f9ff20320ac7c574e09108823b
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-19 08:34:44 -0700

    Committing task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * main - check code
  *
- * 0-reset_to_98.c - reset_to_98 - function that takes a pointer to an int as
+ * reset_to_98.c - reset_to_98 - function that takes a pointer to an int as
  * parameter and updates the value it points to to 98
  *
  * @n: This is the entry
