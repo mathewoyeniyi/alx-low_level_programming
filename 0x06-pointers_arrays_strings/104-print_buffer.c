e14852303127b02a36fe10bf53c760d674aefce6
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-22 12:22:09 -0700

    Committing task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,20 +12,39 @@
 
 void print_buffer(char *b, int size);
 {
-	int i;
+	int x, i;
 
-	for (i = 0; i <= (size - 1) / 10 && size)
+	for (x = 0; x < size; x += 10)
 	{
-		printf("%08x: ", i * 1);
-		if (i < size / 10)
+		printf("%08x: ", x);
+
+		for (i = 0; i < 10; i++)
 		{
-			print_line(b, 9, i);
+			if ((i + x) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + i + x));
+
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
 		}
-		else
+		for (i = 0; i < 10; i++)
 		{
-			print_line(b, size % 10 - 1, i);
+			if ((i + x) >= size)
+				break;
+
+			else if (*(b + i + x) >= 31 && *(b + i + x) <= 126)
+				printf("%c", *(b + i + x));
+
+			else
+				printf(".");
 		}
-		putchar('\n');
+		if (x >= size)
+			continue;
+
+		printf("\n");
 	}
-	if (size == 0)
+	if (size <= 0)
+		printf("\n");
 }
