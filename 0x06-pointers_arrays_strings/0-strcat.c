9234b4215e9beb39d3df59bd7f9727d4ca3c1880
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-22 08:24:38 -0700

    Committing task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,35 +1,20 @@
 #include "main.h"
 
 /**
+ * _strcat - concatenates two strings
+ * @dest: the resulting string
+ * @src: string to add to @dest
+ * Return: @dest
  *
- *  * _strcat - concatenates two strings
- *
- *   * @dest: the resulting string
- *
- *    * @src: string to add to @dest
- *
- *     * Return: @dest
- *
- *      */
+ */
 
 char *_strcat(char *dest, char *src)
-
 {
+	int i, len;
 
-		int i, len;
-
-
-
-			for (len = 0; dest[len] != '\0'; len++)
-
-						;
-
-				for (i = 0; src[i] != '\0'; i++)
-
-							dest[len + i] = src[i];
-
-					dest[len + i] = '\0';
-
-						return (dest);
-
+	for (len = 0; dest[len] != '\0'; len++)
+		;
+	for (i = 0; src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+	return (dest);
 }
