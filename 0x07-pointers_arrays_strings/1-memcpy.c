6c19913c5657a29074dbe6f54f68ac28cc7cdbc2
mathewoyeniyi <oyeniyi.matthew@gmail.com>
2022-09-26 08:29:31 -0700

    Commit task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,21 +3,19 @@
 /**
  * _memcpy - Copies @n bytes from the memory area pointed
  *
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
+ * @dest: char
+ * @src: char
+ * @n: int
  *
- * Return: A pointer to the destination buffer @dest.
+ * Return: char pointer
  */
 
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
