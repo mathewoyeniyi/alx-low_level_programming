09c7180f2fff5a391fb158a70fc056eb13bef4bc
Mathew Oyeniyi <oyeniyi.matthew@gmail.com>
2022-09-26 07:07:00 -0700

    Commit task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - Write a function that fills memory with a constant byte
- * The _memset() function fills the first n bytes of the memory area pointed to
- * by s with the constant byte b
- *
- * @s: This is the output and the return
- * @b: This is the this is the input string
+ * _memset - fill memory with a constant byte
+ * @s:char
+ * @b:char
  * @n:unsigned int
- *
- * Return: car
+ * Return:char
  *
  */
 
@@ -17,10 +13,8 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (count = 0; count < n ; count++)
-	{
-		for (i = 0; i < n; i++)
-			s[i] = b;
-	}
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
 	return (s);
 }
