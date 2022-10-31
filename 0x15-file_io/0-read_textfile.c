9fb3180ae835ab652c9f8fa12d798a1ddd45d6d2
mathewoyeniyi <oyeniyi.matthew@gmail.com>
2022-10-31 22:46:05 +0100

    Commit Task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,37 @@
-#include "main.h"
-#include <stdlib.h>
+#include "maim.h"
 
 /**
  * read_textfile - read text and print it to POSIX
  * @filename: name of file char
  * @letters:number of letters to read size_t
- * Return: read the file display it or error
+ * Return: read the file display it or error 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
 		return (0);
 	}
 
-	free(buffer);
-	close(0);
-
-	return (w);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (wd);
 }
